d5cec87b0a47f23e5930ae17786bbcf693a70cc8
willowchan <e.wingus@gmail.com>
2014-05-10 20:05:46 +0800

    Create Course Project

---
+++ plot2.R
@@ -0,0 +1,16 @@
+## Loading the data
+# Read file into data
+data <- read.table(file="./Data/household_power_consumption.txt", sep=";", colClasses="character")
+# Modify data to normal format since names are not correct
+names(data) <- data[1,]
+data <- data[2:nrow(data),]
+# Subset data for data from the dates 2007-02-01 and 2007-02-02
+subdata <- data[as.character(as.Date(data$Date, format="%d/%m/%Y")) == "2007-02-01" |
+                        as.character(as.Date(data$Date, format="%d/%m/%Y")) == "2007-02-02",  ]
+## Making Plots
+hist(as.numeric(subdata$Global_active_power), col="red", 
+     main="Global Active Power", xlab="Global Active Power (kilowatts)")
+# Copy current display into "plot2.png"
+dev.copy(png, file="plot2.png")
+# Turn off device
+dev.off()
