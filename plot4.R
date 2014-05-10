f132fe2bc246fdd457cd358965e20a112fc7f744
willowchan <e.wingus@gmail.com>
2014-05-11 00:23:59 +0800

    Update plot4.R and plot4.png

--- plot4.R
+++ plot4.R
@@ -25,9 +25,9 @@ plot(subdata2$DateTime, subdata2$Sub_metering_1, type="l", xlab="",
      ylab="Energy sub metering")
 points(subdata2$DateTime, subdata2$Sub_metering_2, type="l", col = "red")
 points(subdata2$DateTime, subdata2$Sub_metering_3, type="l", col = "blue")
-legend("topright", cex=0.3, lty=1, col=c("black", "red", "blue"), 
+legend("topright", pt.cex=0.9, cex=0.7, lty=1, col=c("black", "red", "blue"), 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       border = "white")
+       border = "white", bty = "n", y.intersp=0.2)
 # Fourth plot
 plot(subdata2$DateTime, subdata2$Global_reactive_power, type="l", xlab="datetime", 
      ylab="Global_reactive_power")
